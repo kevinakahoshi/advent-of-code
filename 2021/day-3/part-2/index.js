625e1a2a71c01368d32fffaa0ecb8921d39fe0d5
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2021-12-03 22:39:18 -0800

    Updated variable name

--- 2021/day-3/part-2/index.js
+++ 2021/day-3/part-2/index.js
@@ -5,18 +5,18 @@ fs.readFile('../input.txt', (err, data) => {
 
   const filterByBit = (arr, index, bit) => arr.filter((val) => val[index] === bit);
 
-  const findLifeSupportValue = (binaryArray, column, searchValue) => {
+  const findLifeSupportValue = (binaryArray, index, searchValue) => {
     if (binaryArray.length === 1) return binaryArray[0];
 
     let ones = 0;
     let zeroes = 0;
 
-    binaryArray.forEach((row) => parseInt(row[column]) ? ones++ : zeroes++);
+    binaryArray.forEach((row) => parseInt(row[index]) ? ones++ : zeroes++);
 
     const nextBit = (searchValue && ones >= zeroes) || (!searchValue && zeroes > ones) ? '1' : '0';
-    const filteredList = filterByBit(binaryArray, column, nextBit);
+    const filteredList = filterByBit(binaryArray, index, nextBit);
 
-    return findLifeSupportValue(filteredList, column + 1, searchValue);
+    return findLifeSupportValue(filteredList, index + 1, searchValue);
   }
 
   const oxygen = findLifeSupportValue(input, 0, 1);
