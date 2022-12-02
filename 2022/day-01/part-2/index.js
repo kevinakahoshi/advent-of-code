e3a3a59b3d27d8e48523a3f5dd77cc975af199c5
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2022-12-01 21:53:04 -0800

    Finished part 2

--- 2022/day-01/part-2/index.js
+++ 2022/day-01/part-2/index.js
@@ -1,6 +1,21 @@
 const fs = require('fs');
 
 fs.readFile('../input.txt', (err, data) => {
-  const input = data.toString().split('\n').map((val) => parseInt(val));
-  console.log(input);
-})
+  const values = data.toString().split('\n').map((val) => parseInt(val));
+
+  const totals = [0];
+
+  values.forEach((value) => {
+    const last = totals.length - 1;
+    if (isNaN(value)) {
+      totals.push(0);
+    } else {
+      totals[last] += value;
+    }
+  });
+
+  const topThree = totals.sort((val1, val2) => val2 - val1).slice(0, 3).reduce((accum, curr) => accum + curr, 0);
+  console.table({
+    topThree
+  });
+});
