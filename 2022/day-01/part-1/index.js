467047bb2ac67183289f1b45e46514e09787289e
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2022-12-01 21:59:29 -0800

    Used reduce instead

--- 2022/day-01/part-1/index.js
+++ 2022/day-01/part-1/index.js
@@ -3,25 +3,14 @@ const fs = require('fs');
 fs.readFile('../input.txt', (err, data) => {
   const values = data.toString().split('\n').map((val) => parseInt(val));
 
-  const totals = [0];
-  let max = 0;
-  let maxIndex = 0;
+  const totals = values.reduce((accum, curr) => {
+    if (isNaN(curr)) return [...accum, 0];
 
-  values.forEach((value) => {
-    const last = totals.length - 1;
-    if (isNaN(value)) {
-      totals.push(0);
-    } else {
-      totals[last] += value;
-    }
+    const last = accum.length - 1;
+    accum[last] += curr;
 
-    if (totals[last] > max) {
-      max = totals[last];
-      maxIndex = last;
-    }
-  });
+    return accum;
+  }, [0]);
 
-  console.table({
-    max,
-  })
+  console.log(Math.max(...totals));
 });
--- 2022/day-01/part-2/index.js
+++ 2022/day-01/part-2/index.js
@@ -3,18 +3,17 @@ const fs = require('fs');
 fs.readFile('../input.txt', (err, data) => {
   const values = data.toString().split('\n').map((val) => parseInt(val));
 
-  const totals = [0];
+  const totals = values.reduce((accum, curr) => {
+    if (isNaN(curr)) return [...accum, 0];
 
-  values.forEach((value) => {
-    const last = totals.length - 1;
-    if (isNaN(value)) {
-      totals.push(0);
-    } else {
-      totals[last] += value;
-    }
-  });
+    const last = accum.length - 1;
+    accum[last] += curr;
+
+    return accum;
+  }, [0]);
 
   const topThree = totals.sort((val1, val2) => val2 - val1).slice(0, 3).reduce((accum, curr) => accum + curr, 0);
+
   console.table({
     topThree
   });
