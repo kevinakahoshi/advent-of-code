4533d1b8f1b91fccb4c6a59f07669475d6ce4a75
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2022-12-04 22:59:30 -0800

    Completed part 2

--- 2022/day-04/part-2/index.js
+++ 2022/day-04/part-2/index.js
@@ -12,8 +12,10 @@ fs.readFile('../input.txt', (err, data) => {
     const [e1Start, e1End] = elf1.split('-').map(Number);
     const [e2Start, e2End] = elf2.split('-').map(Number);
 
-    if (e1Start >= e2Start && e1End <= e2End) return true;
-    if (e2Start >= e1Start && e2End <= e1End) return true;
+    if (e1Start >= e2Start && e1Start <= e2End) return true;
+    if (e2Start >= e1Start && e2Start <= e1End) return true;
+    if (e1End >= e2Start && e1End <= e2End) return true;
+    if (e2End >= e1Start && e2End <= e1End) return true;
     return false;
   });
 
