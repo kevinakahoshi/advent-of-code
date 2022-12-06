fab1da65de01c93651ac37e62d720bbdfc6bdeae
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2022-12-05 23:06:05 -0800

    Whoops

--- 2022/day-05/part-2/index.js
+++ 2022/day-05/part-2/index.js
@@ -1,23 +1,78 @@
 const fs = require('fs');
 
+class Stack {
+  constructor(id) {
+    this.data = [];
+    this.id = id;
+  }
+
+  pop() {
+    return this.data.pop();
+  }
+
+  push(value) {
+    this.data.push(value);
+  }
+
+  peek() {
+    return this.data[this.data.length - 1];
+  }
+}
+
+const formatCrate = (crate) => {
+  if (crate) return crate.trim();
+  return '';
+}
+
+const generateStacks = (crates) => {
+  const stacks = [...Array(9).keys()].map((value) => value + 1);
+  const rowIdString = [...crates].pop();
+  const rowCrateStrings = [...crates].slice(0, -1).reverse();
+
+  return stacks.map((column, index) => {
+    const indexOfCrateColumn = rowIdString.indexOf(column.toString());
+    const stack = new Stack(column);
+    let currentIndex = 0;
+
+    for (const row of rowCrateStrings) {
+      const crate = formatCrate(row[indexOfCrateColumn]);
+      if (!crate) break;
+      stack.push(crate);
+    }
+
+    return stack;
+  });
+}
+
+const formatInstructions = (instructions) => {
+  return instructions
+    .replace('move', '')
+    .replace('from', ',')
+    .replace('to', ',')
+    .split(',')
+    .map(Number);
+}
+
 fs.readFile('../input.txt', (err, data) => {
-  const pairs = data.toString()
-    .trim()
-    .split('\n')
-    .map((pair) => pair.split(','));
+  if (err) throw err;
+  const input = data.toString().split('\n');
 
-  const fullyContainedPairs = pairs.filter((pair) => {
-    const [elf1, elf2] = pair;
+  const crates = input.slice(0, 9);
+  const stacks = generateStacks(crates);
 
-    const [e1Start, e1End] = elf1.split('-').map(Number);
-    const [e2Start, e2End] = elf2.split('-').map(Number);
+  const allInstructions = input.slice(10).map(formatInstructions);
 
-    if (e1Start >= e2Start && e1Start <= e2End) return true;
-    if (e2Start >= e1Start && e2Start <= e1End) return true;
-    if (e1End >= e2Start && e1End <= e2End) return true;
-    if (e2End >= e1Start && e2End <= e1End) return true;
-    return false;
+  allInstructions.forEach(([total, from, to], index) => {
+    let count = total;
+
+    while (count > 0) {
+      const crate = stacks[from - 1].pop();
+      stacks[to - 1].push(crate);
+      count--;
+    }
   });
 
-  console.log(fullyContainedPairs.length);
+  const topOfEachStack = stacks.map((stack) => stack.peek());
+
+  console.log(topOfEachStack.join(''));
 });
