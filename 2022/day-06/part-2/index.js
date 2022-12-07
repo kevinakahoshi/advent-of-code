71723dcd2256a105d051cce65516b44ab74faf0f
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2022-12-06 22:04:30 -0800

    Completed part 2

--- 2022/day-06/part-2/index.js
+++ 2022/day-06/part-2/index.js
@@ -3,10 +3,12 @@ const fs = require('fs');
 fs.readFile('../input.txt', (err, data) => {
   const input = data.toString();
 
-  for (let start = 0, end = 4; end < input.length; start++, end++) {
+  const markerSize = 14;
+
+  for (let start = 0, end = markerSize; end < input.length; start++, end++) {
     const characters = new Set([...input.slice(start, end)]);
 
-    if (characters.size === 4) {
+    if (characters.size === markerSize) {
       console.table({
         end
       });
