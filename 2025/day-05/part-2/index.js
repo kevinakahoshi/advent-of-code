5b6e0fd1781ad38b30721b2b49f32b6ec3e9b616
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2025-12-10 21:50:24 -0800

    Updating part 2

--- 2025/day-05/part-2/index.js
+++ 2025/day-05/part-2/index.js
@@ -3,52 +3,23 @@ const fs = require('fs');
 const fileName = 'input';
 const file = `../${fileName}.txt`;
 
-const recursivePaperRemoval = (matrix) => {
-  const count = matrix.reduce((total, row, rIndex) => {
-    return (
-      total +
-      row.reduce((rowTotal, col, cIndex) => {
-        if (col === '.') return rowTotal;
-
-        const prevRow = rIndex - 1;
-        const nextRow = rIndex + 1;
-        const prevCol = cIndex - 1;
-        const nextCol = cIndex + 1;
-
-        const isValid =
-          [
-            matrix[prevRow]?.[prevCol],
-            matrix[prevRow]?.[cIndex],
-            matrix[prevRow]?.[nextCol],
-            row[prevCol],
-            row[nextCol],
-            matrix[nextRow]?.[prevCol],
-            matrix[nextRow]?.[cIndex],
-            matrix[nextRow]?.[nextCol],
-          ].filter((char) => char === '@').length < 4;
-
-        if (isValid) {
-          matrix[rIndex][cIndex] = '.';
-        }
-
-        return rowTotal + Number(isValid);
-      }, 0)
-    );
-  }, 0);
-
-  return count ? count + recursivePaperRemoval(matrix) : count;
-};
-
 fs.readFile(file, (err, data) => {
   if (err) console.error(err);
 
-  const input = data
+  const [idRangeStrs, availableIdStrs] = data
     .toString()
     .trim()
-    .split('\n')
-    .map((row) => row.split(''));
+    .split('\n\n')
+    .map((val) => val.split('\n'));
+  const idRanges = idRangeStrs.map((val) => val.split('-').map(Number));
+  const availableIds = availableIdStrs.map(Number);
 
-  const count = recursivePaperRemoval(input);
+  const count = availableIds.reduce((total, id) => {
+    const isFresh = !!idRanges.find(
+      (range) => range.at(0) <= id && id <= range.at(1)
+    );
+    return total + Number(isFresh);
+  }, 0);
 
   console.table({ count });
 });
